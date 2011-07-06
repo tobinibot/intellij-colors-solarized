471f9de5ce09d0ca98bfd5b0629ca26fae770404
Tobin Juday <tobinibot@gmail.com>
2011-07-06 08:55:30 -0400

    added scripts to copy theme files around

---
+++ deploy.sh
@@ -0,0 +1,4 @@
+#/bin/sh
+cp 'Solarized Dark.xml' ~/Library/Preferences/IntelliJIdea10/colors/
+cp 'Solarized Light.xml' ~/Library/Preferences/IntelliJIdea10/colors/
+
---
+++ update.sh
@@ -0,0 +1,4 @@
+#/bin/sh
+cp '~/Library/Preferences/IntelliJIdea10/colors/Solarized Dark.xml' .
+cp '~/Library/Preferences/IntelliJIdea10/colors/Solarized Light.xml' .
+
